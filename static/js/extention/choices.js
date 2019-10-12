0550ec9ef0065530fed44868596a0a776493b646
Aditya Dutta <aditya.dutta10@gmail.com>
2019-10-12 16:38:34 -0400

    -language search works

--- static/js/extention/choices.js
+++ static/js/extention/choices.js
@@ -1,12 +1,15 @@
-$('.btn-search').click(function() {
-    let className = document.getElementById('search').value;
-    let lang = document.getElementById('language-choice');
-    lang = lang.options[lang.selectedIndex].text;
-    console.log(lang);
-    $.ajax({
-        type: 'GET',
-        url: '/' + className + '/' + lang
-    }).done(function(res) {
-        console.log('Worked!');
-    });
-});
+// $('.btn-search').click(function() {
+//     let className = document.getElementById('search').value;
+//     let lang = document.getElementById('language-choice');
+//     lang = lang.options[lang.selectedIndex].text;
+//     console.log(lang);
+//     // $.ajax({
+//     //     type: 'GET',
+//     //     url: '/' + className + '/' + lang
+//     // }).done(function(res) {
+//     //     console.log('Worked!');
+//     // });
+//     $.post( '/' + className + '/' + lang, {
+//         //javascript_data: data 
+//     });
+// });
