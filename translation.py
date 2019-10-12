15e555888ba96c72534803fa686e52cfd3bcff4c
Bill Bai <billbai0102@gmail.com>
2019-10-12 01:52:29 -0400

    Updated translation

--- translation.py
+++ translation.py
@@ -5,8 +5,11 @@ def createTranslation(text, language):
     translate_client = translate.Client()
     language = iso.to_iso639_1(language)
 
-    translation = translate_client.translate(
-        text,
-        target_language=language
-    )
+    try:
+        translation = translate_client.translate(
+            text,
+            target_language=language
+        )
+    except: 
+        return 'Unfortunately, that language is not supported.'
     return translation['translatedText']
