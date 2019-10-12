a5796a6f191d1c02aaa35a50b2ceb79ee102c33b
Bill Bai <billbai0102@gmail.com>
2019-10-12 12:00:56 -0400

    Database restructured

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, request
 import getsearch
+from database import DatabaseManager
+import firebase_admin
+from firebase_admin import credentials
+from twilio.twiml.messaging_response import MessagingResponse
+import translation
 
 app = Flask(__name__)
+
+if (not len(firebase_admin._apps)):
+    cred = credentials.Certificate('static/mhacks12-22906-firebase-adminsdk-lbt7e-d7c9a27bb5.json') 
+    default_app = firebase_admin.initialize_app(cred,  {'databaseURL': 'https://mhacks12-22906.firebaseio.com/'})
+
+lang = ""
+previous = ""
+
+dm = DatabaseManager("super_notes")
 
 @app.route('/')
 def main():
@@ -9,11 +23,32 @@ def main():
 
 @app.route("/index")
 def index():
-    return render_template('index.html')
-
+    data = get_supernote("Physics")
+    user = "Bill"
+    return render_template('index.html', data=data, user=user)
 @app.route('/pass_val',methods=['POST'])
 def pass_val():
     name=request.args.get('value')
     print('name',name)
     print(getsearch.getInfoByHash('-LqyiulvtclaFSFsC4_Q')[2])
+
+
+"""
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    resp = MessagingResponse()
+    lang = str(resp)
+    resp.message(translation.createTranslation("Enter a course you wish to learn about.", language=lang))
+    return str(resp)
+    # # Add a message
+    # resp.message("Hello")
+
+    # return str(resp)
+    
+"""
+
+def get_supernote(course):
+    cl = dm.find_notes_by_course_name(course)
+    return cl
     
