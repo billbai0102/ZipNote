e326cf5326b2e9ceff2b43c27bf8e8cf726a5962
Emerson Pfeiffer <epfeiffe@umich.edu>
2019-10-12 23:14:58 -0400

    Added language switching functionality with google API

--- static/js/extention/choices.js
+++ static/js/extention/choices.js
@@ -10,6 +10,32 @@
 //     //     console.log('Worked!');
 //     // });
 //     $.post( '/' + className + '/' + lang, {
-//         //javascript_data: data 
+//         //javascript_data: data
 //     });
 // });
+
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement(
+//         { pageLanguage: 'en' },
+//         'google_translate_element'
+//     );
+// }
+
+// function switchLang() {
+//     let language = document.getElementById('language-choice');
+//     language = language.options[language.selectedIndex].value;
+//     console.log(language.lang);
+//     console.log(language);
+//     if (language === 'Afrikanns') {
+//         console.log('yes');
+//         console.log(document.getElementsByClassName('goog-te-combo')[0]);
+//         document.getElementsByClassName('goog-te-combo')[0].value = 'af';
+//     }
+//     window.location.location = '/index.html.ru';
+// }
+
+// document.getElementsByClassName('skiptranslate')[0].innerHTML = '';
+// let l = document.getElementByClassName('goog-te-combo')[0];
+// console.log(l);
+// document.getElementByClassName('goog-te-combo')[0].options[0].value =
+//     'Language';
